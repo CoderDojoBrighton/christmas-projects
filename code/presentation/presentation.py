23b5b6b5e9a9274046336697493e82d9117760fa
Bradley Garrod <bradleybredgarod@gmail.com>
2023-12-15 20:28:52 +0000

    feat: Add snowflakes

--- code/presentation/presentation.py
+++ code/presentation/presentation.py
@@ -1,4 +1,4 @@
-import christmas_tree, title
+import christmas_tree, title, snowflake
 import turtle
 
 width = 500
@@ -11,6 +11,15 @@ speed = 2
 while True:
     s.clear()
     title.draw(speed)
+    # snowflake.draw(speed+2, 210)
+    snowflake.draw(600, 190)
+    snowflake.draw(400, -80)
+    snowflake.draw(600, 30)
+    snowflake.draw(800, -40)
+    snowflake.draw(800, 160)
+    snowflake.draw(900, -10)
+    snowflake.draw(300, 130)
+    snowflake.draw(750, 230)
     christmas_tree.draw(speed)
 
 turtle.mainloop()
